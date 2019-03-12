155e798f563e8e9e80752457badb84e5d8282623
Bas Stroosnijder <bas@strootje.com>
2019-03-12 22:03:41 +0100

    added tests for searchbox

--- test/Browser.test.ts
+++ test/Browser.test.ts
@@ -1,8 +1,8 @@
 import { expect } from 'chai';
 import { ChildProcess, spawn } from 'child_process';
 import { join } from 'path';
-import { launch as LaunchChrome } from 'puppeteer';
-import { launch as LaunchFirefox } from 'puppeteer-firefox';
+import { launch as LaunchChrome, Page as ChromePage } from 'puppeteer';
+import { launch as LaunchFirefox, Page as FirefoxPage } from 'puppeteer-firefox';
 import * as ResembleJS from 'resemblejs';
 
 interface ScreenResolution {
@@ -18,37 +18,45 @@ const res: { [_ in Resolutions]: ScreenResolution } = {
 	mobile: { width: 320, height: 480 }
 };
 
-async function captureFirefox(url: string, dims: Resolutions): Promise<string> {
+async function captureFirefox(url: string, dims: Resolutions, moveTo?: (page: FirefoxPage) => Promise<void>): Promise<string> {
 	const path = join(__dirname, `firefox-${url.replace(/\//g, '_')}-${dims}.jpg`);
 
 	const browser = await LaunchFirefox({ defaultViewport: res[dims] });
 	const page = await browser.newPage();
 	await page.goto(`http://localhost:1313${url}`);
-	await page.screenshot({ fullPage: true, path: path });
 
+	if (moveTo) {
+		await moveTo(page);
+	}
+
+	await page.screenshot({ fullPage: true, path: path });
 	await browser.close();
 
 	return path;
 }
 
-async function captureChrome(url: string, dims: Resolutions): Promise<string> {
+async function captureChrome(url: string, dims: Resolutions, moveTo?: (page: ChromePage) => Promise<void>): Promise<string> {
 	const path = join(__dirname, `chrome-${url.replace(/\//g, '_')}-${dims}.jpg`);
 
 	const browser = await LaunchChrome({ defaultViewport: res[dims], args: [ '--no-sandbox', '--disable-setuid-sandbox' ] });
 	const page = await browser.newPage();
 	await page.goto(`http://localhost:1313${url}`);
-	await page.screenshot({ fullPage: true, path: path });
 
+	if (moveTo) {
+		await moveTo(page);
+	}
+
+	await page.screenshot({ fullPage: true, path: path });
 	await browser.close();
 
 	return path;
 }
 
-function runTest(threshold: number, url: string, dims: Resolutions): Mocha.AsyncFunc {
+function runTest(threshold: number, url: string, dims: Resolutions, moveTo?: (page: ChromePage) => Promise<void>): Mocha.AsyncFunc {
 	return async (): Promise<void> => {
 		// Arrange
-		const ffPath = await captureFirefox(url, dims);
-		const chPath = await captureChrome(url, dims);
+		const ffPath = await captureFirefox(url, dims, moveTo);
+		const chPath = await captureChrome(url, dims, moveTo);
 
 		// Act
 		const compare = ResembleJS(ffPath)
@@ -84,6 +92,24 @@ describe('Compare <firefox> and <chrome> browser:', () => {
 		it('should look the same on `desktop` resolution', runTest(0.4, '/', 'desktop')).timeout(0);
 		it('should look the same on `tablet` resolution', runTest(0.6, '/', 'tablet')).timeout(0);
 		it('should look the same on `mobile` resolution', runTest(0.9, '/', 'mobile')).timeout(0);
+	});
+
+	describe('The Homepage with Searchbox', () => {
+		const searchSelector = '.widget-search input[type=\'search\']';
+		const selectSearchBox = async (page: ChromePage) => {
+			await page.type(searchSelector, 'test');
+			await page.waitFor(1000);
+		};
+
+		it('should look the same on `fullhd` resolution', runTest(0.2, '/#search', 'fullhd', selectSearchBox)).timeout(0);
+		it('should look the same on `desktop` resolution', runTest(0.4, '/#search', 'desktop', selectSearchBox)).timeout(0);
+		it('should look the same on `tablet` resolution', runTest(0.7, '/#search', 'tablet', selectSearchBox)).timeout(0);
+		it('should look the same on `mobile` resolution', runTest(1.1, '/#search', 'mobile', async (page: ChromePage) => {
+			await page.click('.navbar-burger');
+			await page.waitForSelector(searchSelector);
+			await selectSearchBox(page);
+		})).timeout(0);
+
 	});
 
 	describe('The Singlepage', () => {

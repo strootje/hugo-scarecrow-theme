4e17262948a2b8f5a1ac51e2cef0dfb1739c0aed
Bas Stroosnijder <bas@strootje.com>
2019-03-12 23:27:38 +0100

    appvoyer just extract hugo

--- test/Browser.test.ts
+++ test/Browser.test.ts
@@ -78,7 +78,7 @@ describe('Compare <firefox> and <chrome> browser:', () => {
 	let server: ChildProcess;
 
 	before(() => {
-		server = spawn('pnpm', ['run', 'serve'], { detached: true });
+		server = spawn('npm', ['run', 'serve'], { detached: true });
 	});
 
 	after(() => {

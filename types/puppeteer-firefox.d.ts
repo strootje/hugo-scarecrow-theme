d232fdb18cebc99bcc1a6dd3390142d6f6d735d9
Bas Stroosnijder <bas@strootje.com>
2019-03-13 10:34:39 +0100

    rewriten tests to be faster

--- types/puppeteer-firefox.d.ts
+++ types/puppeteer-firefox.d.ts
@@ -1,5 +1,5 @@
 declare module 'puppeteer-firefox' {
 	import { Browser, LaunchOptions } from 'puppeteer';
-	export { Page } from 'puppeteer';
+	export { Browser, Page } from 'puppeteer';
 	export function launch(options: LaunchOptions): Browser;
 }
